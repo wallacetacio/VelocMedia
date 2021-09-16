9c6ad0060cce240b7b2fa8d589d1eac1f9abc1d4
Wallace Tácio <wallacett.dev@gmail.com>
2021-09-16 18:48:59 -0300

    Adiciona resolução ao exercício Velocidade Média ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string distancia, tempo;
+
+            Console.WriteLine(" ");
+            Console.WriteLine(@"Olá usuário,
+pronto para calcular a velocidade média? Vamos lá!");
+            Console.WriteLine(" ");
+
+            Console.Write("Distância percorrida (m): ");
+            distancia = Console.ReadLine();
+             Console.WriteLine(" ");
+
+            Console.Write("Tempo gasto (s): ");
+            tempo = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            double d = Convert.ToDouble(distancia);
+            double t = Convert.ToDouble(tempo);
+
+            double Vm = d / t;
+
+            Console.WriteLine($"Velocidade média: {Vm}m/s");
+            Console.WriteLine(" ");
+
+            if(Vm >= 27)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+             Console.WriteLine("Você está acima de 100Km/h! Cuidado!");
+             Console.ResetColor();   
+            }else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Continue assim! ;)");
+                Console.ResetColor();
+            }
+        }
+    }
+}
